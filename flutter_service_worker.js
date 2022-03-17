05f6655346d58dacb5059b9928e385493b5d323e
vulong230996 <vulongit102@gmail.com>
2022-03-17 22:33:32 +0700

    Hoàn thiện add thông tin

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "970f158f62ba1bdb52b731de5b431610",
-"index.html": "d1297a217c1845d60ac1e16784b2b5f9",
-"/": "d1297a217c1845d60ac1e16784b2b5f9",
-"main.dart.js": "b89c07631f7a2616524b7992a281a25f",
+"index.html": "04cb1f3732b1a255a8195524049a3e6b",
+"/": "04cb1f3732b1a255a8195524049a3e6b",
+"main.dart.js": "2c68b743fc85c37dc3f5f7743cee04f3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
