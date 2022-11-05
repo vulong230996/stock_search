f11a75ebe5ebbc54acdab940ba4d90b50dfaa5f0
vulong230996 <vulongit102@gmail.com>
2022-11-05 16:30:16 +0700

    Add files via upload
    upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "970f158f62ba1bdb52b731de5b431610",
-"index.html": "3923e3d626bf6704f6a7af9f0d1c0d77",
-"/": "3923e3d626bf6704f6a7af9f0d1c0d77",
-"main.dart.js": "a44caba020acddc055aeb3e283753bf1",
+"index.html": "89743f83a413bfbccd7523918ec540ea",
+"/": "89743f83a413bfbccd7523918ec540ea",
+"main.dart.js": "48503a2725d5d3e75549730499dbeab4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
